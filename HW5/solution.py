4a7cea39661d6d087880ef52cfc9780ec81ee939
goo-gy <lkukh17@naver.com>
2022-05-05 00:19:57 +0900

    comment : HW5

--- HW5/solution.py
+++ HW5/solution.py
@@ -61,18 +61,20 @@ def KeypointProjection(xy_points, h):
     # START
     h_points = []
     for xy_point in xy_points:
+        # image coordinate를 homogeneous coordinate로 변환
         h_point = np.append(xy_point, 1)
         h_points.append(h_point)
     h_points = np.array(h_points)
 
     xy_points_out = []
     for h_point in h_points:
+        # homography matrix를 곱하여 reference coordinate로 변환
         xy_point_out = h.dot(h_point)
         nomalizer = xy_point_out[2] if (xy_point_out[2] != 0) else 1e-10
+        # additional dimension을 없애고 그 값으로 나누어 projection
         xy_point_out = xy_point_out[:2] / nomalizer
         xy_points_out.append(xy_point_out)
     xy_points_out = np.array(xy_points_out)
-
     # END
     return xy_points_out
 
@@ -105,35 +107,40 @@ def RANSACHomography(xy_src, xy_ref, num_iter, tol):
     tol = tol * 1.0
 
     # START
-    minSetNum = 4
+    minMatchCount = 4  # homography matrix를 구하기 위해 최소 4개의 match가 필요
     maxCount = 0
     maxMatches = 0
     for k in range(num_iter):
         matches = []
-        # pick random 4 matches
-        for _ in range(minSetNum):
+        # random으로 4개의 match 선택
+        for _ in range(minMatchCount):
             match = random.randint(0, len(xy_src) - 1)
             matches.append(match)
+        # 선택한 match로 homography matrix를 구하고 proejction
         h, _ = cv2.findHomography(xy_src[matches], xy_ref[matches])
         xy_proj = KeypointProjection(xy_src, h)
 
         inlierCount = 0
+        # 모든 match를 순회하여 inlier counting
         for i in range(xy_proj.shape[0]):
             dist = np.linalg.norm(xy_proj[i] - xy_ref[i])
             if dist < tol:
                 inlierCount += 1
+        # inlier가 가장 많은 match set 선택
         if inlierCount > maxCount:
             maxCount = inlierCount
             maxMatches = matches
-    # ----------------------------------
+
+    # inlier가 가장 많은 match set으로 homography matrix를 구하고 proejction
     h, _ = cv2.findHomography(xy_src[maxMatches], xy_ref[maxMatches])
     xy_proj = KeypointProjection(xy_src, h)
+    # inlier 추출
     inliers = []
     for i in range(xy_proj.shape[0]):
         dist = np.linalg.norm(xy_proj[i] - xy_ref[i])
         if dist < tol:
             inliers.append(i)
-
+    # inlier를 가지고 다시 homography matrix를 구해서 return
     h, _ = cv2.findHomography(xy_src[inliers], xy_ref[inliers])
     # END
     assert isinstance(h, np.ndarray)
