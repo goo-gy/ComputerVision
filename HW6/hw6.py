994bfb0df285aced056970e0b538563c4159ac0c
goo-gy <lkukh17@naver.com>
2022-05-22 01:15:49 +0900

    problem : hW6

---
+++ HW6/hw6.py
@@ -0,0 +1,78 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+img1 = plt.imread('./data/warrior_a.jpg')
+img2 = plt.imread('./data/warrior_b.jpg')
+
+cor1 = np.load("./data/warrior_a.npy")
+cor2 = np.load("./data/warrior_b.npy")
+
+
+def compute_fundamental(x1, x2):
+    n = x1.shape[1]
+    if x2.shape[1] != n:
+        exit(1)
+
+    F = None
+    # YOUR CODE BEGINS HERE
+
+    # build matrix for equations in Page 51
+
+    # compute the solution in Page 51
+
+    # constrain F: make rank 2 by zeroing out last singular value (Page 52)
+
+    # YOUR CODE ENDS HERE
+
+    return F
+
+
+def compute_norm_fundamental(x1, x2):
+    n = x1.shape[1]
+    if x2.shape[1] != n:
+        exit(1)
+
+    # normalize image coordinates
+    x1 = x1 / x1[2]
+    mean_1 = np.mean(x1[:2], axis=1)
+    S1 = np.sqrt(2) / np.std(x1[:2])
+    T1 = np.array([[S1, 0, -S1*mean_1[0]], [0, S1, -S1*mean_1[1]], [0, 0, 1]])
+    x1 = T1 @ x1
+
+    x2 = x2 / x2[2]
+    mean_2 = np.mean(x2[:2], axis=1)
+    S2 = np.sqrt(2) / np.std(x2[:2])
+    T2 = np.array([[S2, 0, -S2*mean_2[0]], [0, S2, -S2*mean_2[1]], [0, 0, 1]])
+    x2 = T2 @ x2
+
+    # compute F with the normalized coordinates
+    F = compute_fundamental(x1, x2)
+
+    # reverse normalization
+    F = T2.T @ F @ T1
+
+    return F
+
+
+def compute_epipoles(F):
+    e1 = None
+    e2 = None
+    # YOUR CODE BEGINS HERE
+    pass
+    # YOUR CODE ENDS HERE
+
+    return e1, e2
+
+
+def draw_epipolar_lines(img1, img2, cor1, cor2):
+    F = compute_norm_fundamental(cor1, cor2)
+
+    e1, e2 = compute_epipoles(F)
+    # YOUR CODE BEGINS HERE
+    pass
+    # YOUR CODE ENDS HERE
+
+    return
+
+
+draw_epipolar_lines(img1, img2, cor1, cor2)
