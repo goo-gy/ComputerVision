5c43fc5bb47d9bcdf1500a8191f43a0053aacae6
goo-gy <lkukh17@naver.com>
2022-06-03 16:29:20 +0900

    comments : HW7

--- HW7/solution.py
+++ HW7/solution.py
@@ -14,17 +14,21 @@ l_pC2 = [10, 0.1]
 
 def prob1():
     for row, pRandomState in enumerate(l_pRandomState):
+        # data 생성
         coords, labels = datasets.make_blobs(
             n_samples=100, cluster_std=1.2, random_state=pRandomState, centers=2)
 
-        plt.figure(figsize=(15, 5))
+        plt.figure(figsize=(16, 5))
         for col, pC in enumerate(l_pC1):
             plt.subplot(1, 3, col + 1)
             ax = plt.gca()
             ax.set_title('C=a%.1f' % pC)
 
+            # liner kernel을 이용해 SVM classifier 생성
             clf = SVC(kernel="linear", C=pC)
             clf.fit(coords, labels)
+
+            # SVM classifier model을 통해 line 그래프 생성
             inspection.DecisionBoundaryDisplay.from_estimator(
                 clf,
                 coords,
@@ -34,6 +38,7 @@ def prob1():
                 ax=ax,
             )
 
+            # label에 따라 분리하여 scatter 그래프 생성
             plt.scatter(coords[labels == 0, 0], coords[labels == 0, 1])
             plt.scatter(coords[labels == 1, 0], coords[labels == 1, 1])
             plt.scatter(clf.support_vectors_[:, 0], clf.support_vectors_[
@@ -42,9 +47,11 @@ def prob1():
 
 
 def createDatasets():
+    # data 생성
     coords, labels = datasets.make_circles(
         n_samples=100, factor=0.1, noise=0.1)
 
+    # label에 따라 분리하여 scatter 그래프 생성
     plt.scatter(coords[labels == 0, 0], coords[labels == 0, 1])
     plt.scatter(coords[labels == 1, 0], coords[labels == 1, 1])
     plt.show()
@@ -52,6 +59,7 @@ def createDatasets():
 
 
 def gauss_rbf(coords):
+    # 2차원 coordinates를 gaussian rbf를 통해 3차원으로 확장
     X = coords[:, 0]
     Y = coords[:, 1]
     Z = np.exp(-(X**2 + Y**2))
@@ -63,6 +71,7 @@ def kernelFunction(coords, labels):
     ax = fig.add_subplot(projection='3d')
     X, Y, Z = gauss_rbf(coords)
 
+    # label에 따라 분리하여 scatter 그래프 생성
     ax.scatter(X[labels == 0], Y[labels == 0], Z[labels == 0])
     ax.scatter(X[labels == 1], Y[labels == 1], Z[labels == 1])
     plt.show()
@@ -75,8 +84,11 @@ def trainSVM(coords, labels):
         ax = plt.gca()
         ax.set_title('C=%.1f' % pC)
 
+        # rbf kernel을 이용해 SVM classifier 생성
         clf = SVC(kernel="rbf", C=10)
         clf.fit(coords, labels)
+
+        # SVM classifier model을 통해 line 그래프 생성
         inspection.DecisionBoundaryDisplay.from_estimator(
             clf,
             coords,
@@ -86,6 +98,7 @@ def trainSVM(coords, labels):
             ax=ax,
         )
 
+        # label에 따라 분리하여 scatter 그래프 생성
         plt.scatter(coords[labels == 0, 0], coords[labels == 0, 1])
         plt.scatter(coords[labels == 1, 0], coords[labels == 1, 1])
     plt.show()
